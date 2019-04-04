0a4c1381d8055a6c5f3800a2d7ec16e4903993e6
Abraham Galindo <oxer_@hotmail.com>
2019-04-03 20:13:28 -0600

    Project created

---
+++ src/webparts/lists/components/IListsProps.ts
@@ -0,0 +1,3 @@
+export interface IListsProps {
+  description: string;
+}
---
+++ src/webparts/lists/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IListsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ListsWebPartStrings' {
+  const strings: IListsWebPartStrings;
+  export = strings;
+}
